32e0eb5853605964082a605bfa30e18e3f640e25
kuwa <h.kuwabara1103@gmail.com>
2026-02-16 22:23:29 +0900

    update debug config

--- learn_loop_app/android/app/build.gradle.kts
+++ learn_loop_app/android/app/build.gradle.kts
@@ -30,10 +30,17 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
